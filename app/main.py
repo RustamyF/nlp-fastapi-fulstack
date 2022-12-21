01052ecf0171a3f92611ca5d0be45eb7225263d5
Fahim Rustamy <faheemrustamy@gmail.com>
2022-12-21 16:43:41 -0500

    added mongo db example

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 from fastapi import FastAPI
 
-from app.db import database, User
+# from dotenv import dotenv_values
+from pymongo import MongoClient
+from app.routes import router as book_router
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+# config = dotenv_values(".env")
+from app.config import settings
 
-
-@app.get("/")
-async def read_root():
-    return await User.objects.all()
+app = FastAPI()
 
 
 @app.on_event("startup")
-async def startup():
-    if not database.is_connected:
-        await database.connect()
-    # create a dummy entry
-    await User.objects.get_or_create(email="test@test.com")
+def startup_db_client():
+    app.mongodb_client = MongoClient(settings.db_url)
+    app.database = app.mongodb_client[settings.db_name]
 
 
 @app.on_event("shutdown")
-async def shutdown():
-    if database.is_connected:
-        await database.disconnect()
+def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+@app.get("/health")
+def list_books():
+
+    return {"result": "hello world"}
+
+
+app.include_router(book_router, tags=["books"], prefix="/book")
